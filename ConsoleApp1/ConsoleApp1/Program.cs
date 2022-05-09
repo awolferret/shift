b4b25f734c76ae1bc04dd3ba527ff66e6fcbf832
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-09 21:44:52 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4 };
+            Console.WriteLine("на сколько сдаинуть массив?");
+            int shiftNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Оригинальный массив");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            for (int i = 0; i < shiftNumber; ++i)
+            {
+                int bufferNumber = array[0];
+
+                for (int j = 1; j < array.Length; j++)
+                {
+                    array[j - 1] = array[j];
+                }
+                array[array.Length - 1] = bufferNumber;
+            }
+            Console.WriteLine("\nНовый массив");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+        }
+    }
+}
